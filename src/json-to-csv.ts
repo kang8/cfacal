b91992bd6988e79c8f61d1a93401ccecffe5193c
kang <yi1115610574@gmail.com>
2023-11-18 23:07:19 +0800

    feat(fetch): Support incremental fetch data

--- src/json-to-csv.ts
+++ src/json-to-csv.ts
@@ -4,6 +4,7 @@ import { Body, CinemaInfo, Movie, MovieHall } from './types.ts'
 import { csvDir } from './csv-to-ics.ts'
 
 const cfaApi = 'https://yt5.cfa.org.cn/api/libraryapi/arrangementPage'
+const patchFileName = 'diff.patch'
 export const csvHeader = [
   'name',
   'englishName',
@@ -75,10 +76,97 @@ export async function fetchJsonAndConvertToCsv(firstDayOfMonth: Date) {
 
   sortByPlayTime(movies)
 
-  Deno.writeTextFile(
-    `${csvDir}/${dateFormat(firstDayOfMonth, 'yyyy-MM')}.csv`,
-    csvStringify(movies, {
-      columns: csvHeader,
-    }),
-  )
+  const filePath = `${csvDir}/${dateFormat(firstDayOfMonth, 'yyyy-MM')}.csv`
+
+  try {
+    await Deno.lstat(filePath)
+
+    Deno.writeTextFile(
+      filePath + '.new',
+      csvStringify(movies, {
+        columns: csvHeader,
+      }),
+    )
+
+    // diff
+    const diffCommand = new Deno.Command('diff', {
+      args: [
+        filePath,
+        filePath + '.new',
+      ],
+    })
+
+    const { stdout } = await diffCommand.output()
+    const diffOutput = new TextDecoder().decode(stdout)
+
+    console.log(diffOutput); // always output diff message for debugging
+
+    const diffOutputs = diffOutput.split('\n')
+
+    let patch = ''
+
+    if (diffOutputs[0].includes('d')) { // delete
+      const range = diffOutputs[0].split('d')[0].split(',')
+      const from = parseInt(range[0])
+      const to = parseInt(range[1])
+
+      const deleteLine = to - from + 1 // add from itself
+
+      diffOutputs.splice(0, deleteLine + 1) // delete from 0 index
+
+      patch = diffOutputs.join('\n')
+    } else if (diffOutputs[0].includes('c')) { // changes
+      const [oldRange, newRange] = diffOutputs[0].split('c')
+
+      const oldFrom = parseInt(oldRange.split(',')[0])
+      const oldTo = parseInt(oldRange.split(',')[1])
+
+      const newFrom = parseInt(newRange.split(',')[0])
+      const newTo = parseInt(newRange.split(',')[1] ?? newFrom)
+
+      const changeLine = newTo - newFrom + 1 // add newFrom itself
+      const deleteLine = oldTo - oldFrom + 1 - changeLine // add oldFrom itself and subtract changeLine
+
+      diffOutputs.splice(1, deleteLine)
+      diffOutputs[0] = `${oldTo - changeLine + 1},${oldTo}c${newFrom},${newTo}`
+
+      patch = diffOutputs.join('\n')
+    } else {
+      console.info('Without any change!!!');
+    }
+
+    Deno.writeTextFile(patchFileName, patch)
+
+    // followed by https://github.com/denoland/deno/blob/c213ad380f349dee1f65e6d9a9f7a8fa669b2af2/cli/tests/unit/command_test.ts#L206-L233
+    // TODO: May be have a sort way: https://github.com/denoland/deno/blob/c213ad380f349dee1f65e6d9a9f7a8fa669b2af2/cli/tests/unit/command_test.ts#L56-L84
+    const patchCommand = new Deno.Command('patch', {
+      args: [
+        filePath,
+      ],
+      stdin: 'piped',
+    })
+
+    const child = patchCommand.spawn();
+
+    const file = await Deno.open(patchFileName);
+    await file.readable.pipeTo(child.stdin, {
+      preventClose: true
+    })
+
+    await child.stdin.close();
+
+    Deno.remove(patchFileName)
+    Deno.remove(filePath + '.new')
+  } catch (error) {
+    if (!(error instanceof Deno.errors.NotFound)) {
+      throw error
+    }
+
+    Deno.writeTextFile(
+      filePath,
+      csvStringify(movies, {
+        columns: csvHeader,
+      }),
+    )
+  }
 }
